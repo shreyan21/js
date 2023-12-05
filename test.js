fdf28dd5a49668aa9b95baf56f00c5f0e6eea334
Utkarsh <rajawatshrayansh@gmail.com>
2023-12-05 23:29:02 +0530

    message print

--- test.js
+++ test.js
@@ -1,7 +1,1 @@
-let x
-console.log(typeof x);
-
-// string which can't be converted to number has value NaN
-// true => 1 ; false=>0 
-// "33abc" => NaN
-// "" => false , "hello"=>true
+console.log("how is it going");
